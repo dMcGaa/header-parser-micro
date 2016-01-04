d3f3d808fdb066783ee2ff0d0af40c11a55fe02f
dmcgaa <mcgaa.david@gmail.com>
2016-01-04 20:48:11 +0000

    request header parsed

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+var browserInfo = {
+  ipaddress: null,
+  language: null,
+  software: null
+}
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -9,10 +14,42 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// Give Views/Layouts direct access to data.
+app.use(function(req, res, next) {
+  res.locals.browserInfo = browserInfo;
+  next();
+});
+
+
 app.get('/', function(request, response) {
+  getBrowserInfo(request.headers);
+  // browserInfo.ipaddress = request.headers['x-forwarded-for'];
+  // browserInfo.language = request.headers['accept-language'];
+  // browserInfo.software = request.headers['user-agent'];
+  // console.log(browserInfo.ipaddress);
+  // console.log(request.headers['accept-language']);
+  // console.log(request.headers['user-agent']);
   response.render('pages/index')
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+function getBrowserInfo(headers){
+  var tempVar;
+  
+  browserInfo.ipaddress = headers['x-forwarded-for'];
+  
+  tempVar = headers['accept-language'].split(",");
+  tempVar = tempVar[0];
+  browserInfo.language = tempVar;
+  
+  
+ tempVar = headers['user-agent'].match(/\([^)]*/); // /((\().+(\))){1}/
+ tempVar = tempVar[0].slice(1);
+  browserInfo.software = tempVar;
+  console.log(browserInfo.ipaddress);
+  console.log(browserInfo.language);
+  console.log(browserInfo.software);
+}
